32fa142b4ee51245821da8649478e2ec3dcf20f9
Matt Jewell <m@mattjewell.co.uk>
2016-06-06 23:02:13 +0100

    get product titles

--- src/runtime/index.js
+++ src/runtime/index.js
@@ -1,7 +1,8 @@
 import httpPageGetter from "../httpPageGetter";
+import getTextContent from "../getNodesTextContent";
 
 export default async function (config) {
-    const httpData = await httpPageGetter("http://www.google.com");
-    console.log("Got the body!");
-    console.log(httpData.body);
+    const httpData = await httpPageGetter(config.scrapeUrl);
+    const textNodes = getTextContent(httpData, [".product h3 a"]);
+    console.log(textNodes);
 };
